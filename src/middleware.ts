bc5ce4051ee5cfc1b71a038580f9fff4aff82ac2
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-09-05 16:22:55 +0000

    switch next

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export function onRequest (context, next) {
   if (context.url.pathname.startsWith('/page-1')) {
-    return context.rewrite('/page-2')
+    return next('/page-2')
   }
   return next()
 }
